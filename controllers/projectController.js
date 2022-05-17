0560de0193b0d4b3b3f049764602cd5eff810959
borismg04 <borismg04@gmail.com>
2022-05-17 00:09:12 -0500

    model project

---
+++ controllers/projectController.js
@@ -0,0 +1,27 @@
+const obtainAllProjects = async (req, res) => {
+
+}
+
+const newProject = async (req, res) => {
+
+}
+
+const handlesObtainProject = async (req, res) => {
+
+}
+
+const handlerEditProject = async (req, res) => {
+
+}
+
+const handlerDeleteProject = async (req, res) => {
+
+}
+
+const handlerAddCollaborator = async (req, res) => {
+
+}
+
+const handlerRemoveCollaborator = async (req, res) => {
+
+}
